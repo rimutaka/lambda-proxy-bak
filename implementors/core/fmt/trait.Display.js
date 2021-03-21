30a014eb072fee7332b8aa3d426fe25977b30f66
rimutaka <rimutaka@users.noreply.github.com>
2021-03-21 03:15:49 +0000

    deploy: 4a2cbb5f46c2ab5a70e847e7c5fa783b9ae0befe

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["lambda_http"] = [{"text":"impl Display for PayloadError","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["lambda"] = [{"text":"impl StructuralPartialEq for Context","synthetic":false,"types":[]},{"text":"impl StructuralPartialEq for Config","synthetic":false,"types":[]}];
+implementors["lambda_http"] = [{"text":"impl StructuralPartialEq for Body","synthetic":false,"types":[]},{"text":"impl StructuralPartialEq for StrMap","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
